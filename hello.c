72fc7b64e7af948759751774e4dcb11a67ab6951
temur arustashvili <tarus19@freeuni.edu.ge>
2025-09-09 00:26:14 +0400

    add simple hello program

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void printHi();
+
+int main(int nargs, char** args) {
+    printf("hello world\n");
+    printHi();
+    return 0;
+}
+
---
+++ hi.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void printHi() {
+	printf("hi\n");
+}
